7d399c5eadc4ef3e84dec6c80f2fc40ba02a7f36
Jainil Sutaria <jsutaria@stanford.edu>
2019-04-13 21:15:36 -0700

    Adding google auth

--- home.js
+++ home.js
@@ -6,61 +6,45 @@
   storageBucket: "trust-drive-8c229.appspot.com",
   messagingSenderId: "431444258807"
 };
+
 if( !firebase.apps.length){
   var app = firebase.initializeApp(config);
 }
-this.ui = firebaseui.auth.AuthUI.getInstance() 
-|| new firebaseui.auth.AuthUI(firebase.auth());
-var uiConfig = {
-  callbacks: {
-    signInSuccessWithAuthResult: function(authResult, redirectUrl) {
-      // User successfully signed in.
-      // Return type determines whether we continue the redirect automatically
-      // or whether we leave that to developer to handle.
-      console.log("TEST");
-      login();
-      return false;
-  },
-  uiShown: function() {
-      // The widget is rendered.
-      // Hide the loader.
 
-    document.getElementById('loader').style.display = 'none';
-  }
-  },
-  // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
-  signInFlow: 'popup',
-  signInSuccessUrl: 'http://localhost:1337/rider.html',
-  signInOptions: [
-    // Leave the lines as is for the providers you want to offer your users.
-    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-  ],
-  // Terms of service url.
-  tosUrl: '<your-tos-url>',
-  // Privacy policy url.
-  privacyPolicyUrl: '<your-privacy-policy-url>'
-};
+var provider = new firebase.auth.GoogleAuthProvider();
 
-
-ui.start('#firebaseui-auth-container-lender', {
-
-  signInSuccessUrl: 'http://localhost:1337/lender.html',
-  signInOptions: [
- // List of OAuth providers supported.
-  firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-  ],
-  // Other config options... 
-});
-
-window.onload = function() {
-  alert("hi");
-  var db = firebase.firestore(app);
-  var docRef = db.collection("Users").doc("michaelhanyy");
-  docRef.get().then(function(doc) {
-    console.log(doc.data());
-    alert(doc.get('has_account'));
+function fb_login() {
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = result.credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    login(user);
+    console.log(user.email);
+    console.log(user.displayName);
+    // ...
+  }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
   });
-};
+}
+
+//
+// window.onload = function() {
+//   alert("hi");
+//   var db = firebase.firestore(app);
+//   var docRef = db.collection("Users").doc("michaelhanyy");
+//   docRef.get().then(function(doc) {
+//     console.log(doc.data());
+//     alert(doc.get('has_account'));
+//   });
+// };
 
 function login() {
   alert("hi");
