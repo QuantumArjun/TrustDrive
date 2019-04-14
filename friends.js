5e967d6a73f942738d1a5e7597db5b9ad87a985a
rmalde <47470168+rmalde@users.noreply.github.com>
2019-04-14 08:20:06 -0700

    stable Ronak

--- friends.js
+++ friends.js
@@ -1,26 +1,49 @@
 var db = firebase.firestore();
 
-function updateFriends() {
 
-    var email = sessionStorage.getItem("email");
+function updateFriends() {
+    // Create a reference to the cities collection
     var doc_element = document.getElementById('deck');
-    alert(email);
-    var documentReference = db.collection('Users').doc(email);
-    documentReference.get().then(function(documentSnapshot) {
-        if (documentSnapshot.exists) {
-            var data = documentSnapshot.data();
-            var friends = data.friends;
-            friends.forEach(function(element) {
+    var db = firebase.firestore();
+    var Users = db.collection("Users");
+    var email = sessionStorage.getItem("email");
+    var thisUser = Users.doc(sessionStorage.getItem("email"));
+    var scheduleRef = thisUser.collection("friends");
+
+
+    scheduleRef.get().then(function(result) {
+        if (result.size > 0) {
+            result.forEach(function (documentSnapshot) {
+                var data = documentSnapshot.data();
+                console.log(data);
+
                 var card = "<div class=\"card\">\n" +
-                "<h3 class=\"friendName\">" + element + "</h3>\n" +
+                    "<h3 class=\"distance\">" + data.name + "</h3>\n" +
                 "   </div>";
                 doc_element.innerHTML += card;
             });
         } else {
-            console.log('document not found');
+            console.log('no documents found');
         }
+
     });
-    updateCarInfo();
+
+    console.log(scheduleRef);
+
+    // Create a query against the collection.
+    // var query = scheduleRef.where("username", "==", "undefined");
+
+    // var qs = query.get().then(function(result) {
+    //     result.forEach(function(e) {
+    //         console.log(e);
+    //     });
+    //     console.log(result.size);
+    // });
+
+
+
+    // console.log(query.get());
+
 }
 
 function updateCarInfo() {
@@ -33,4 +56,46 @@ function updateCarInfo() {
         emails += elements.item(i).innerText;
     }
     console.log(emails);
+}
+
+function openModal() {
+    // Get the modal
+    var modal = document.getElementById('create');
+
+// Get the button that opens the modal
+    var btn = document.getElementById("floating-button");
+
+// Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+    modal.style.display = "block";
+
+// When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+// When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+}
+
+function createFriend () {
+    var doc_element = document.getElementById('deck');
+    var email = sessionStorage.getItem("email");
+    var friendEmail = document.getElementById("submit").value;
+    var db = firebase.firestore();
+    db.collection("Users").doc(email).collection("friends").doc(friendEmail).set({
+        name: name
+    })
+        .then(function() {
+            console.log("Document successfully written!");
+        })
+        .catch(function(error) {
+            console.error("Error writing document: ", error);
+        });
 }
