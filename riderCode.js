fc7651c4b839747e9cdd13235b1af20c19e1051f
Arjun Karanam <akaranam@stanford.edu>
2019-04-14 01:23:44 -0700

    Added rider cards

--- riderCode.js
+++ riderCode.js
@@ -9,28 +9,58 @@ thisUser.get().then(function(doc) {
   clientId = doc.get('Client_ID');    
 });
 
-function updateCars() {
+function updatePage() {
+    // Create a reference to the cities collection
+var doc_element = document.getElementById('overall_container');
 
-    var car_name = document.getElementById("car-name");
-    car_name.innerHTML = "db";
+var query = Users.where("lendsCar", "==", true);
 
-    var car_model = document.getElementById("car-model");
-    car_name.innerHTML = "db";
+    
+    query.get().then(function(result) {
+        if (result.size > 0) {
+          result.forEach(function (documentSnapshot) {
+            var data = documentSnapshot.data();
+            console.log(data);
 
-    var car_distance = document.getElementById("car-distance");
-    car_name.innerHTML = "db";
+            var card = "<div class=\"container\">\n" +
+        "    <h3 class=\"title\">Arjun Karanam</h3>\n" +
+        "    <h3 class=\"distance\">" + data.carModel + "</h3>\n" +
+        "<h3 class = \"carModel\" id = \"car-model\"> Toyota Camry </h3>" + 
+        "   </div>";
+        doc_element.innerHTML += card;
+            });
+        } else {
+          console.log('no documents found');
+        }
+ 
+    });
+
+
+    // Create a query against the collection.
+    // var query = scheduleRef.where("lendCar", "==", true);
+
+    // var qs = query.get().then(function(result) {
+    //     result.forEach(function(e) {
+    //         console.log(e);
+    //     });
+    //     console.log(result.size);
+    // });
+
+
+
+    // console.log(query.get());
 
 }
 
-const smartcar = new Smartcar({
-  clientId: clientId,
-  redirectUri: '<your-redirect-uri>',
-  scope: ['read_vehicle_info', 'read_odometer'],
-  onComplete: function(err, code) {
-    if (err) {
-      // handle errors from the authorization flow (i.e. user denies access)
-    }
-    // handle the returned code by sending it to your back-end server
-    sendToBackend(code);
-  },
-});
+//const smartcar = new Smartcar({
+//   clientId: clientId,
+//   redirectUri: '<your-redirect-uri>',
+//   scope: ['read_vehicle_info', 'read_odometer'],
+//   onComplete: function(err, code) {
+//     if (err) {
+//       // handle errors from the authorization flow (i.e. user denies access)
+//     }
+//     // handle the returned code by sending it to your back-end server
+//     sendToBackend(code);
+//   },
+// });
