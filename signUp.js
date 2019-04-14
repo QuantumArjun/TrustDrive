fb797634eadb68bbc009de52f66183935379308b
rmalde <47470168+rmalde@users.noreply.github.com>
2019-04-13 19:08:15 -0700

    signup

--- signUp.js
+++ signUp.js
@@ -1,3 +1,25 @@
+var db = firebase.firestore();
+
 function writeToFirebase() {
+
+    var carModel = document.getElementById("model").value;
+    var address = document.getElementById("address").value;
+    var user = sessionStorage.getItem("username");
+    user = "Ronak";
+    var inUse = false;
+    var pricePerHour = 14.50;
+
+    db.collection("Users").add({
+        model: carModel,
+        address: address,
+        inUse: inUse,
+        pricePerHour: pricePerHour
+    })
+        .then(function(docRef) {
+            console.log("Document written with ID: ", docRef.id);
+        })
+        .catch(function(error) {
+            console.error("Error adding document: ", error);
+        });
 
 }
