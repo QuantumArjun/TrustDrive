80902daf5b779e76daff641e67e3ec6178c0fbba
rmalde <47470168+rmalde@users.noreply.github.com>
2019-04-13 20:13:48 -0700

    signup not working with firebase

--- signUp.js
+++ signUp.js
@@ -1,25 +1,29 @@
+var firebase = require("firebase");
 var db = firebase.firestore();
 
-function writeToFirebase() {
 
+function writeToFirebase() {
     var carModel = document.getElementById("model").value;
     var address = document.getElementById("address").value;
     var user = sessionStorage.getItem("username");
     user = "Ronak";
     var inUse = false;
     var pricePerHour = 14.50;
+    alert("Point1");
 
-    db.collection("Users").add({
-        model: carModel,
-        address: address,
-        inUse: inUse,
-        pricePerHour: pricePerHour
+
+    db.collection("Users").doc("michaelhanyy").set({
+        model: "EOGIN",
+        address: "sldgn",
+        inUse: false,
+        pricePerHour: 8
     })
-        .then(function(docRef) {
-            console.log("Document written with ID: ", docRef.id);
+
+        .then(function() {
+            console.log("Document successfully written!");
         })
         .catch(function(error) {
-            console.error("Error adding document: ", error);
+            console.error("Error writing document: ", error);
         });
-
+    alert("Point1");
 }
