67dd43429135c6a98986a76ed2d41f622ecbbb46
Arjun Karanam <akaranam@stanford.edu>
2019-04-14 04:55:30 -0700

    undid bad stuff

--- friends.js
+++ friends.js
@@ -1,17 +1,16 @@
 var db = firebase.firestore();
-var emails= [];
 
 function updateFriends() {
 
     var email = sessionStorage.getItem("email");
     var doc_element = document.getElementById('deck');
+    alert(email);
     var documentReference = db.collection('Users').doc(email);
     documentReference.get().then(function(documentSnapshot) {
         if (documentSnapshot.exists) {
             var data = documentSnapshot.data();
             var friends = data.friends;
             friends.forEach(function(element) {
-                emails.push(element);
                 var card = "<div class=\"card\">\n" +
                 "<h3 class=\"friendName\">" + element + "</h3>\n" +
                 "   </div>";
@@ -25,5 +24,13 @@ function updateFriends() {
 }
 
 function updateCarInfo() {
+    var elements = document.getElementsByClassName("friendName");
+    console.log(elements);
+    var emails= [];
+    for (i = 0; i < elements.length; i++) {
+        console.log(i);
+        console.log(elements.item(i));
+        emails += elements.item(i).innerText;
+    }
     console.log(emails);
 }
